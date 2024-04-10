71f415977bec96c8565283b9bb06970320eff7cf
Spyjm <156465344+Spyjm@users.noreply.github.com>
2024-04-09 23:25:45 -0400

    Merging of Reviews with Text.py

--- Merging of Reviews with Text.py
+++ Merging of Reviews with Text.py
@@ -6,11 +6,15 @@ file1_df = pd.read_csv("sentiment_and_reviews.csv")
 # Read the second CSV file into a DataFrame
 file2_df = pd.read_csv("restaurant_reviews_with_sentiment.csv")
 
+# Remove duplicates from the 'text' column in each DataFrame
+file1_df = file1_df.drop_duplicates(subset='text')
+file2_df = file2_df.drop_duplicates(subset='text')
+
 # Merge the DataFrames based on the 'text' column
 merged_df = pd.merge(file1_df, file2_df, on='text', how='inner')
 
 # Remove specified columns
-columns_to_remove = ['user_id', 'time', 'pics', 'resp']
+columns_to_remove = ['user_id_y', 'time_y', 'pics_y', 'resp_y']
 merged_df = merged_df.drop(columns=columns_to_remove, errors='ignore')
 
 # Create a new column for the difference between 'Sentiment_x' and 'Sentiment_y'
