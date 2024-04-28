58b30c431ea08df9db4b57be621f521e153cf483
Spyjm <156465344+Spyjm@users.noreply.github.com>
2024-04-28 17:08:59 -0400

    Sentiment Boxplots.py

--- Sentiment Histograms.py
+++ Sentiment Histograms.py
@@ -7,23 +7,25 @@ merged_df = pd.read_csv("merged_data.csv")
 # Get unique rating values
 unique_ratings = merged_df['rating_y'].unique()
 
-# Create subplots for each histogram
+# Create subplots for each boxplot
 fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
 
-# Plot histograms for each rating and save as separate PNG files
+# Plot boxplots for each rating and save as separate PNG files
 for i, ax in enumerate(axes.flatten()):
     rating = unique_ratings[i]
     filtered_df = merged_df[merged_df['rating_y'] == rating]
-    ax.hist(filtered_df['Sentiment_y'], bins=20, color='skyblue', edgecolor='black')
+    ax.boxplot(filtered_df['Sentiment_y'], vert=False)
     ax.set_title(f'Rating {rating}')
     ax.set_xlabel('Sentiment')
-    ax.set_ylabel('Frequency')
+    ax.set_ylabel('Rating')
+    ax.grid(True)
 
-    # Save the histogram as a separate PNG file
-    plt.savefig(f'histogram_rating_{rating}.png')
+    # Save the boxplot as a separate PNG file
+    plt.savefig(f'boxplot_rating_{rating}.png')
 
 # Adjust layout
 plt.tight_layout()
 
-# Show the histograms
+# Show the boxplots
 plt.show()
+
